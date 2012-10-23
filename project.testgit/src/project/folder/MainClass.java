c6cd75bf5666757ac85352ac60b6ae3ccc737e85
Sigurd Ulimoen Myhre <sigumyhre@gmail.com>
2012-10-23 13:47:02 +0200

    fdfd
    fdfd

--- project.testgit/src/project/folder/MainClass.java
+++ project.testgit/src/project/folder/MainClass.java
@@ -8,6 +8,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Making a change");
+		System.out.println("Change two");
 	}
 
 }
